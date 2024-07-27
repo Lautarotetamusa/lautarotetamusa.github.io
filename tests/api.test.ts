9dba7bb3b4d017df3ffaf4b314437fc6d938ee46
lautarotetamusa <lautarotetamusa@gmail.com>
2024-07-27 16:57:59 -0300

    test: Api get random word

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,5 +1,9 @@
 import {Game} from "../src/main";
 
 test('Obtener palabra vía api', function () {
-    const game = new Game(() => {});
+    const game = new Game(() => {
+        expect(typeof game.word).toBe("string");
+        expect(game.word).toHaveProperty("length");
+        expect(game.word.length).toBeGreaterThan(0);
+    });
 });
